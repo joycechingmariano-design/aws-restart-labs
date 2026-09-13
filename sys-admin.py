702da4eaa4d7c22500f1ab3a94476952ea8ccafc
joycechingmariano-design <joycechingmariano@gmail.com>
2026-09-14 00:13:37 +0800

    sys-admin.py

---
+++ sys-admin.py
@@ -0,0 +1,26 @@
+import os
+
+os.system("dir")
+import subprocess
+
+subprocess.run(["cmd", "/c", "dir"])
+import subprocess
+
+subprocess.run(["cmd", "/c", "dir"])
+import subprocess
+
+import subprocess
+
+subprocess.run(["cmd", "/c", "dir", "sys-admin.py"])
+import subprocess
+
+command = "systeminfo"
+
+print(f"Gathering system information with command: {command}")
+subprocess.run(["cmd", "/c", command])
+import subprocess
+
+command = "tasklist"
+
+print(f"Gathering active process information with command: {command}")
+subprocess.run(["cmd", "/c", command])
